246dd42fd13cea9cc4f8da8c3dc12126ca7cc038
Levi Donaldson <lcdbacanas@gmail.com>
2017-03-05 19:20:49 -0600

    initial commit

---
+++ main.js
@@ -0,0 +1,17 @@
+var fs = require('fs')
+
+var result = fs.readFileSync('data.txt', 'utf8')
+    .trim()
+    .split('\n')
+    .map(line => line.split(/\s{2,}/))
+    .reduce((customers, line) => {
+        customers[line[0]] = customers[line[0]] || []
+        customers[line[0]].push({
+            rod: line[1],
+            fish: line[2],  
+            quantity: line[3]
+        })
+        return customers
+    }, {})
+
+console.log('result', JSON.stringify(result, null, 2))
